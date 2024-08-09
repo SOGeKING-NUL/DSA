2d34f4bbbfe971b570433ae055de552a7627affa
Utsav Jana <utsav.jana.5@gmail.com>
2024-08-09 09:48:45 +0530

    pp

--- lc_240.cpp
+++ lc_240.cpp
@@ -5,18 +5,28 @@ using namespace std;
 
 class Matrix_2D{
     public:
-        int ppap=3;
-        
-        int search(vector<int> &arr, int target){
 
-        }
+        int search(vector<vector<int>> &arr, int target){
+               
+        int m= arr.size();
+        int n= arr[0].size();
+        int row=0;
+        int cols= n-1;  //starting at the end of the first row of elements
+
+        while(row < m && cols >= 0){
+
+            if(arr[row][cols] == target) return true;
+            else if(arr[row][cols] < target) row++;
+            else cols--;
+        }return false;
+    }
 };
 
 int main(){
-    vector<int> arr{{1,2,3},{4,5,6},{7,8,9},{10,11,12}};
-    int m= arr.size();
-    int n= arr[0].size();
+    vector<vector<int>> arr{{1,2,3},{4,5,6},{7,8,9},{10,11,12}};
 
     Matrix_2D matrix;
-    cout<< matrix.search(arr, 11);
+    bool ans= matrix.search(arr, 22);
+    if (ans == true) cout<< 'T';
+    else cout << 'F';
 }
